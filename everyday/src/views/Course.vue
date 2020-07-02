4fe4dd6884b87c78470cc520d43d2b65ac390111
张存 <3357812514@qq.com>
2020-07-02 18:31:13 +0800

    提交

--- everyday/src/views/Course.vue
+++ everyday/src/views/Course.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="course_container">
-      课程页面
+        <!-- 头部 -->
+      <div class="course_top_contaier">
+       <div class="course_top_left">特色课</div>
+       <div class="course_top_right">
+           <img src="../assets/sousuo.png" class="course_top_right_icon" alt="">
+       </div>
+      </div>
+
+      <!-- 筛选栏 -->
+      <div class="course_sort_container">
+         
+      </div>
     </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
     components: {},
     data() {
@@ -15,9 +26,42 @@ export default {
     },
     computed: {},
     methods: {},
-    mounted() {},
+    mounted() {
+        axios.get('').then((response)=>{
+            window.console.log(response.data)
+        })
+    },
     }
 </script>
-<style  scoped>
+<style>
+.course_container{
+    width:100vw;
+    height: 100vh;
+}
+.course_top_contaier{
+   width:100%;
+   height: 50px;
+   display: flex;
+   justify-content: flex-end;
+   align-items:center;
+   border-bottom:1px solid rgb(228, 228, 228);
+   position:relative;
+}
+.course_top_left{
+    position:absolute;
+    left:45%;
+}
+.course_top_right_icon{
+    width:25px;
+    height: 25px;;
+}
+.course_sort_container{
+    width:100%;
+    height: 50px;
+    background:pink;
+    display: flex;
+    justify-content: space-around;
+    align-items:center;;
+}
 
 </style>
